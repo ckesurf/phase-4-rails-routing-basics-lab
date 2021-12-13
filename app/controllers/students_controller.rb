c0b97e405951b42f57058d18de40ab3a6efbca70
Christopher K Erlendson <christopherkerlendson@Christophers-MacBook-Pro.local>
2021-12-13 13:00:52 -0500

    commit tree c37c8441a6288200f43e64e2d613bbf47f8c2d0f

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,15 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students
+    end
+
+    def grades
+        students_by_grade = Student.all.order(grade: :desc)
+        render json: students_by_grade
+    end
+
+    def highest_grade
+        render json: Student.all.max_by(&:grade)
+    end
+end
